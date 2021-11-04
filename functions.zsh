93b32802108d88e9ae3e0454c8bbd10d7f895170
Camille Scott <camille.scott.w@gmail.com>
2021-11-03 20:26:44 -0700

    zsh configuration migration, vim, git

---
+++ functions.zsh
@@ -0,0 +1,128 @@
+#ZSH_THEME_CONDA_ENV_PROMPT_PREFIX="‹"
+#ZSH_THEME_CONDA_ENV_PROMPT_SUFFIX="› "
+#ZSH_THEME_PY_PROMPT_PREFIX="⟮py"
+#ZSH_THEME_PY_PROMPT_SUFFIX="⟯ "
+HOSTNAME="$(hostname)"  # Conda clobbers HOST, so we save the real hostname into another variable.
+
+precmd() {
+  OLDHOST="${HOST}"
+  HOST="${HOSTNAME}"
+}
+
+preexec() {
+  HOST="${OLDHOST}"
+}
+
+conda_prompt_info() {
+    if [ -n "$CONDA_DEFAULT_ENV" ]; then
+        echo "$ZSH_THEME_CONDA_ENV_PROMPT_PREFIX$CONDA_DEFAULT_ENV$ZSH_THEME_CONDA_ENV_PROMPT_SUFFIX"
+    fi
+}
+
+pyversion() {
+    echo "`python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`"
+}
+
+py_prompt_info() {
+    echo '$ZSH_THEME_PY_PROMPT_PREFIX''$(pyversion)''$ZSH_THEME_PY_PROMPT_SUFFIX'
+}
+
+centerf() {
+  if [[ -n "$2" ]]
+  then
+    padding="$(printf '%0.1s' "$2"{1..500})"
+  else
+    padding="$(printf '%0.1s' ' '{1..500})"
+  fi
+
+  termwidth="$(tput cols)"
+
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+acenterf() {
+    local IFS=$'\n'
+    for line ($=1) centerf "$line" $2
+}
+
+motd_welcome() {
+    centerf "welcome, $USER"
+}
+
+motd_unames() {
+    if [[ `uname -s` == 'Darwin' ]]; then
+        centerf "`uname -srm`"
+    else
+        centerf "`uname -o`"
+        centerf "`uname -r`"
+        centerf "`uname -m`"
+    fi
+}
+
+motd_cpuinfo() {
+    if [[ `uname -s` == 'Darwin' ]]; then
+        centerf "`hostinfo | grep physically`"
+        centerf "`hostinfo | grep logically`"
+    else
+        centerf "`cat /proc/cpuinfo | grep -m 1 "model name" | awk -F":" '{print $2}'`"
+    fi
+}
+
+motd_meminfo() {
+    if [[ `uname -s` == 'Darwin' ]]; then 
+        centerf "`hostinfo | grep memory`"
+    else
+        centerf "`free -m | grep Mem | awk '{print $3 "M of "$2"M RAM used ("$7"M cached)"}'`"
+    fi
+}
+
+motd_dfinfo() {
+    if [[ `uname -s` == 'Darwin' ]]; then 
+        acenterf "`df -h | grep /dev/disk1s1 | awk '{print $3,"of",$2,"("$5") used on "$9}'`"
+    else
+        acenterf "`df -h | grep "data\|home\|\s\/$" | awk '{print $3 " of "$2 " ("$5") on "$6}'`"
+    fi
+}
+
+export COLS=`tput cols`
+
+div() {
+    tput sgr 0; tput bold; tput setaf 4; printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =; tput sgr 0
+}
+
+smalldiv() {
+    tput bold; tput setaf 4; centerf '********************'; tput sgr 0
+}
+
+
+motd() {
+    div
+    echo
+    echo
+    tput setaf 5
+    #figlet -c -w `tput cols` -f Broadway $HOSTNAME
+    tput sgr 0
+    echo
+    echo
+    tput sitm
+    tput setaf 6
+    motd_welcome
+    tput sgr 0
+    echo
+    centerf "`date`"
+    echo
+    motd_unames
+    echo
+    smalldiv
+    echo
+    motd_cpuinfo
+    motd_meminfo
+    echo
+    smalldiv
+    echo
+    motd_dfinfo
+    echo
+    div
+    tput sgr 0
+    echo
+}
--- home.nix
+++ home.nix
@@ -9,6 +9,98 @@
   home.username = "camille";
   home.homeDirectory = "/home/camille";
 
+  home.packages = with pkgs; [
+    figlet
+  ];
+
+  programs.vim.enable = true;
+  home.file.".vimrc".source = ./vimrc;
+
+  programs.htop.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    history.expireDuplicatesFirst = true;
+    history.extended = true;
+    history.save = 10000000;
+    history.size = 10000000;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git-extras"
+        "git"
+        "gitfast"
+        "github"
+        "pip"
+        "ssh-agent"
+        "colorize"
+        "colored-man-pages"
+        "catimg"
+      ];
+      extraConfig = ''
+        zstyle :omz:plugins:ssh-agent agent-forwarding on
+      '';
+      custom = "$HOME/nixfiles/oh-my-zsh";
+      theme = "camillescott";
+    };
+    plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "932e29a0c75411cb618f02995b66c0a4a25699bc";
+          hash = "sha256-gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
+        };
+      }
+    ];
+    sessionVariables = {
+      EDITOR = "vim";
+      GCC_COLORS = "error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01";
+      VIRTUAL_ENV_DISABLE_PROMPT = 1;
+      ZSH_THEME_CONDA_ENV_PROMPT_PREFIX = "‹";
+      ZSH_THEME_CONDA_ENV_PROMPT_SUFFIX = "› ";
+      ZSH_THEME_PY_PROMPT_PREFIX = "⟮py";
+      ZSH_THEME_PY_PROMPT_SUFFIX = "⟯ ";
+
+    };
+    shellAliases = {
+      ".." = "cd ..";
+
+    };
+    initExtraFirst = ''
+      source $HOME/nixfiles/functions.zsh
+    '';
+    initExtra = ''
+    __conda_setup="$('$HOME/miniconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "$HOME/miniconda/etc/profile.d/conda.sh" ]; then
+            . "$HOME/miniconda/etc/profile.d/conda.sh"
+        else
+            export PATH="$HOME/miniconda/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Camille Scott";
+    userEmail = "camille.scott.w@gmail.com";
+    aliases = {
+      graph = "log --oneline --abbrev-commit --all --graph --decorate --color";
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
