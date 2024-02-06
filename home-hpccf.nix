a5635ea9d3839e00190cbcf939329d2951db5474
Camille Scott <camille.scott.w@gmail.com>
2024-02-06 13:46:58 -0800

    hpccf home config

---
+++ home-hpccf.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, ...}: 
+let
+
+  username = "camw";
+  homeDirectory = "/home/camw";
+
+in {
+
+  home.username = username;
+  home.homeDirectory = homeDirectory;
+  home.packages = with pkgs; [
+    tmux
+  ];
+
+  imports = [ ./common ];
+
+  programs.bash = {
+    enable = true;
+    profileExtra = "export XDG_DATA_DIRS=\"$HOME/.nix-profile/share:$XDG_DATA_DIRS\"";
+  };
+
+}
