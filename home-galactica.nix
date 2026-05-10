198f58503d4c60a77589005fc5111d78565b4155
Camille Scott <camille.scott.w@gmail.com>
2026-05-10 13:35:44 -0400

    Move nvidia enablement into galactica config

--- home-galactica.nix
+++ home-galactica.nix
@@ -8,4 +8,10 @@
 
   imports = [ ./linux ./common ./common/kitty ];
 
+  targets.genericLinux.gpu.nvidia = {
+    enable = true;
+    version = "595.71.05";
+    sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+  };
+
 }
--- linux/default.nix
+++ linux/default.nix
@@ -9,11 +9,6 @@ in {
 
   imports = [ ../common/ssh-ident ];
   targets.genericLinux.enable = true;
-  targets.genericLinux.gpu.nvidia = {
-    enable = true;
-    version = "595.71.05";
-    sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
-  };
 
   home.username = username;
   home.homeDirectory = homeDirectory;
