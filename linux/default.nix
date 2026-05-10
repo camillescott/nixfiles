e61640958b53d14deda90ce094e7b4fe64fe17c7
Camille Scott <camille.scott.w@gmail.com>
2026-05-10 13:33:51 -0400

    Bump nvidia driver version

--- linux/default.nix
+++ linux/default.nix
@@ -11,9 +11,9 @@ in {
   targets.genericLinux.enable = true;
   targets.genericLinux.gpu.nvidia = {
     enable = true;
-    version = "590.48.01";
-    sha256 = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
-};
+    version = "595.71.05";
+    sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+  };
 
   home.username = username;
   home.homeDirectory = homeDirectory;
