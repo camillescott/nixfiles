d6ae4cd9c4fd1205563870d7cd002f4b9b4b1c45
Camille Scott <camille.scott.w@gmail.com>
2026-07-27 20:24:53 -0400

    bump nvidia driver version

--- home-yavanna.nix
+++ home-yavanna.nix
@@ -10,8 +10,8 @@
 
   targets.genericLinux.gpu.nvidia = {
     enable = true;
-    version = "595.71.05";
-    sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+    version = "610.43.03";
+    sha256 = "sha256-ReLUwTSiPDXlDyU6SqY+fl6NF+PRhdSgfIpY6WEu05I=";
   };
 
 }
